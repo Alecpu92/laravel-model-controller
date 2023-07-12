470a7bdb76f5a3e748a7deb6660e60fbe008a706
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-07-12 17:59:58 +0200

    passato movies su fil maincontroller

--- app/Http/Controllers/Maincontroller.php
+++ app/Http/Controllers/Maincontroller.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movies;
+
 class Maincontroller extends Controller
 {
     public function index() {
+
+        $movies = Movie :: all();
 
         return view('home');
     }
